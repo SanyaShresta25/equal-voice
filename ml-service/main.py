9cb098897186c6fbefae2e1e8168eea55171869f
SanyaShresta25 <sanyashresta@gmail.com>
2025-07-12 10:45:06 +0530

    fix:deplyment issue

--- ml-service/main.py
+++ ml-service/main.py
@@ -1,1 +1,21 @@
-uvicorn main:app --host 0.0.0.0 --port 10000
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from schema import TextRequest, AnalysisResponse
+from predict import analyze_text
+import nltk
+nltk.download('punkt')
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/analyze", response_model=AnalysisResponse)
+def analyze(request: TextRequest):
+    result = analyze_text(request.text)
+    result["inputText"] = request.text
+    return result
